51a2892984250071858786ce9d9bf7572f03994c
Jiajun Sun <jiajun@aerialintel.com>
2017-03-08 13:25:05 -0800

    decoder now works

--- code/decoder.py
+++ code/decoder.py
@@ -82,9 +82,7 @@ class LSTM_Decorder(Decoder):
 
 
 class BiLSTM_Decoder(Decoder):
-
     def model_layer(self, context_mask, G):
-        print('G', G)
         # G (?, 8h, m)
         with tf.variable_scope('model_layer'):
             lstm_fw_cell = tf.nn.rnn_cell.BasicLSTMCell(self.hidden_size, forget_bias=1.0)
@@ -98,8 +96,8 @@ class BiLSTM_Decoder(Decoder):
         return hidden_outputs
 
     def output_layer(self, G, M):
-        print('M', M)
         # M (?, 2h, m)
+        # the softmax part is implemented together with loss function
         with tf.variable_scope('output_layer'):
             w_1 = tf.get_variable('w_start', shape=(10 * self.hidden_size, 1),
                 initializer=tf.contrib.layers.xavier_initializer())
@@ -109,18 +107,14 @@ class BiLSTM_Decoder(Decoder):
             temp_1 = tf.transpose(tf.concat(1, [G, M]), perm=[0, 2, 1])  # (?, m, 10h)
             temp_1_reshape = tf.reshape(temp_1, shape=[-1, 10 * self.hidden_size])  # (?m, 10h)
             h_1 = tf.reshape(tf.matmul(temp_1_reshape, w_1), [-1, self.max_context_len]) # (?m, 10h) * (10h, 1) -> (?m, 1) -> (?, m)
-            p_1 = tf.nn.softmax(h_1, dim=-1)
 
             temp_2 = tf.transpose(tf.concat(1, [G, M]), perm=[0, 2, 1])  # (?, m, 10h)
             temp_2_reshape = tf.reshape(temp_2, shape=[-1, 10 * self.hidden_size])  # (?m, 10h)
             h_2 = tf.reshape(tf.matmul(temp_2_reshape, w_2), [-1, self.max_context_len]) # (?m, 10h) * (10h, 1) -> (?m, 1) -> (?, m)
-            p_2 = tf.nn.softmax(h_2, dim=-1)
-            print(p_1)
-            print(p_2)
 
-            return p_1, p_2
+            return h_1, h_2
 
     def decode(self, context_mask, G):
         M = self.model_layer(context_mask, G)
-        p1, p2 = self.output_layer(G, M)
-        return p1, p2
+        h1, h2 = self.output_layer(G, M)
+        return h1, h2
