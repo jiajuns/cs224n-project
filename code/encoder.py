2f3a1284c398635f48d2d5cf2b0933440da3f8a0
Jiajun Sun <jiajun@aerialintel.com>
2017-03-08 14:54:52 -0800

    modify encoder try to improve performance

--- code/encoder.py
+++ code/encoder.py
@@ -61,7 +61,7 @@ class BiLSTM_Encoder():
             H = self.Q2C_attention(y_q, y_c, S)  # H = (?, 2h, m)
             U = self.C2Q_attention(y_q, y_c, S)  # U = (?, 2h, m)
             # need to compute G
-            G = tf.concat(1, [y_c, H, y_c * U, y_c * H])
+            G = tf.concat(1, [y_c, H, y_c * U, y_c * H])  # G = (?, 8h, m)
         return G
 
     def similarity(self, y_q, y_c):
@@ -73,7 +73,7 @@ class BiLSTM_Encoder():
                 initializer=tf.contrib.layers.xavier_initializer())
             w_s2 = tf.get_variable('w_sim_2', shape=(2 * self.hidden_size, 1),
                 initializer=tf.contrib.layers.xavier_initializer())
-            w_s3 = tf.get_variable('w_sim_3', shape=(1, 1, 2 * self.hidden_size),
+            w_s3 = tf.get_variable('w_sim_3', shape=(1, 2 * self.hidden_size, 1),
                 initializer=tf.contrib.layers.xavier_initializer())
 
             y_c_T = tf.transpose(y_c, perm=[0, 2, 1])
@@ -81,28 +81,27 @@ class BiLSTM_Encoder():
 
             S_c = tf.matmul(tf.reshape(y_c_T, [-1, 2 * self.hidden_size]), w_s1)  # (?m, 2h) * (2h, 1) = (?m, 1)
             S_q = tf.matmul(tf.reshape(y_q_T, [-1, 2 * self.hidden_size]), w_s2)  # (?n, 2h) * (2h, 1) = (?n, 1)
-            S_c = tf.reshape(S_c, [-1, self.max_context_len, 1])                # (?, m, 1)
+            S_c = tf.reshape(S_c, [-1, 1, self.max_context_len])                # (?, m, 1)
             S_q = tf.reshape(S_q, [-1, self.max_question_len, 1])               # (?, n, 1)
 
-            S_cov = tf.matmul(y_c_T * w_s3, y_q)                                # [(?, m, 2h) o (1, 1, 2h)] * (?, 2h, n) = (?, m, n)
-            S = S_cov + tf.matmul(S_c, tf.transpose(S_q, perm=[0, 2, 1]))       # (?, m, n) + (?, m, 1) * (?, 1, n) = (?, m, n)
+            S_cov = tf.matmul(y_q_T, y_c * w_s3)  # (?, n, 2h) * [(?, 2h, m) o (1, 2h, 1)] = (?, n, m)
+            S = S_cov + tf.matmul(S_q, S_c)       # (?, n, m) + (?, n, 1) * (?, 1, m) = (?, n, m)
         return S
 
     def C2Q_attention(self, y_q, y_c, S):
         # y_q: (?, 2h, n)
         # y_c: (?, 2h, m)
-        # S: (?, m, n)
-        a = tf.nn.softmax(S, dim=1)   # (?, m, n)
-        a = tf.transpose(a, perm=[0, 2, 1])   # (?, n, m)
+        # S: (?, n, m)
+        a = tf.nn.softmax(S, dim=-1)   # (?, n, m)
         U = tf.matmul(y_q, a)    # (?, 2h, n) * (?, n, m) = (?, 2h, m)
         return U
 
     def Q2C_attention(self, y_q, y_c, S):
         # y_q: (?, 2h, n)
         # y_c: (?, 2h, m)
-        # S: (?, m, n)
-        b = tf.nn.softmax(tf.reduce_max(S, axis=2)) # b = (?, m, 1)
-        b = tf.reshape(b, [-1, self.max_context_len, 1])
+        # S: (?, n, m)
+        b = tf.nn.softmax(tf.reduce_max(S, axis=1)) # b = (?, 1, m)
+        b = tf.reshape(b, [-1, self.max_context_len, 1]) # (?, m, 1)
         h = tf.matmul(y_c, b)  # (?, 2h, m) * (?, m, 1) = (?, 2h, 1)
         H = tf.tile(h, [1, 1, self.max_context_len])
         return H
